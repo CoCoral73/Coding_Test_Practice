76141dbf06dea88b9331a62213dfdc8f2d6eaf91
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-07-11 00:51:36 +0900

    [Gold I] Title: 세진이의 미팅, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15791. 세진이의 미팅/세진이의 미팅.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define fastIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+const long long MOD = 1000000007;
+
+long long powering(int x, int y) {
+    if (y == 1)
+        return x;
+    if (y % 2 == 1)
+        return x*powering(x, y-1) % MOD;
+    long long p = powering(x, y/2);
+    return p*p % MOD;
+}
+
+int main() {
+    fastIO;
+
+    int N, M;
+    cin >> N >> M;
+
+    M = min(M, N-M);
+    long long num = 1, deno = 1;
+    for (int i = 0; i < M; i++) {
+        num *= (N-i);
+        num %= MOD;
+        deno *= (M-i);
+        deno %= MOD;
+    }
+    deno = powering(deno, MOD-2);
+    cout << num*deno % MOD;
+
+    return 0;
+}
