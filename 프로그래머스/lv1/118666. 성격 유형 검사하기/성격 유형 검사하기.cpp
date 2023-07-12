c86c2fbd2b3bbb9c313d23324a1471e0ac3e1147
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-07-12 22:17:05 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.07 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -5,28 +5,26 @@ using namespace std;
 
 string solution(vector<string> survey, vector<int> choices) {
     string answer = "";
-    pair<pair<char, char>, int> type[4] = {{{'R', 'T'}, 0}, {{'C', 'F'}, 0},
-                                           {{'J', 'M'}, 0}, {{'A', 'N'}, 0}};
-    int score[8] = {0,3,2,1,0,-1,-2,-3};
+    pair<string, int> type[4] = {{"RT", 0}, {"CF", 0}, {"JM", 0}, {"AN", 0}};
     for (int i = 0; i < survey.size(); i++) {
-        int flag = 1;
+        int flag = -1;
         if (survey[i][0] > survey[i][1])
-            flag = -1;
+            flag = 1;
         if (survey[i] == "RT" || survey[i] == "TR")
-            type[0].second += score[choices[i]]*flag;
+            type[0].second += (choices[i]-4)*flag;
         else if (survey[i] == "CF" || survey[i] == "FC")
-            type[1].second += score[choices[i]]*flag;
+            type[1].second += (choices[i]-4)*flag;
         else if (survey[i] == "JM" || survey[i] == "MJ")
-            type[2].second += score[choices[i]]*flag;
+            type[2].second += (choices[i]-4)*flag;
         else
-            type[3].second += score[choices[i]]*flag;
+            type[3].second += (choices[i]-4)*flag;
     }
 
     for (int i = 0; i < 4; i++) {
         if (type[i].second >= 0)
-            answer += type[i].first.first;
+            answer += type[i].first[0];
         else
-            answer += type[i].first.second;
+            answer += type[i].first[1];
     }
     return answer;
 }
