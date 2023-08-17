94e9c929e81465b3170e4dd73997bfcddcd3b0c6
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-08-17 19:33:34 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 9.59 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ arr:[Int], _ k:Int) -> [Int] {
+    var answer = [Int]()
+    if k % 2 == 1 { answer = arr.map{ $0 * k } }
+    else { answer = arr.map{ $0 + k } }
+    return answer
+}
