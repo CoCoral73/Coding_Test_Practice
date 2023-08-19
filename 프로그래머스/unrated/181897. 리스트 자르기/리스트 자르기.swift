5c9f8d69f1c998000f0c64ce43f8e7914b39a528
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-08-19 14:52:22 +0900

    [unrated] Title: 리스트 자르기, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ n:Int, _ slicer:[Int], _ num_list:[Int]) -> [Int] {
+    switch n {
+    case 1:
+        return Array(num_list[...slicer[1]])
+    case 2:
+        return Array(num_list[slicer[0]...])
+    case 3:
+        return Array(num_list[slicer[0]...slicer[1]])
+    default:
+        var answer = [Int]()
+        for i in stride(from: slicer[0], through: slicer[1], by: slicer[2]) { answer.append(num_list[i]) }
+        return answer
+    }
+}
