76d5b0a463bec911bad90fab8e343859b046e187
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-08-21 22:44:44 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ num_list:[Int]) -> Int {
+    var odd = 0
+    var even = 0
+    for i in 0..<num_list.count {
+        if i % 2 == 0 { even += num_list[i] }
+        else { odd += num_list[i] }
+    }
+    return max(odd, even)
+}
