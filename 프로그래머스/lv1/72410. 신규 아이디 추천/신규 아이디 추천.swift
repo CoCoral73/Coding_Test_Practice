47babdc739a08d25198d60f867220f26fdfa5f23
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-08-24 13:07:28 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.19 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.swift
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.swift
@@ -1,61 +1,22 @@
 import Foundation
 
 func solution(_ new_id:String) -> String {
-    var arr: [Character] = []
-    //step1, 2
-    for ch in new_id {
-        if ch.isLetter {
-            if ch.isUppercase { arr.append(Character(ch.lowercased())) }
-            else { arr.append(ch) }
-        }
-        else if ch.isNumber { arr.append(ch) }
-        else if ch == "-" || ch == "_" || ch == "." { arr.append(ch) }
-    }
-
-    //step3
-    var start = 0, end = 0, i = 0
-    var flag = false
-    while true {
-        if arr[i] == "." {
-            if (!flag) {
-                start = i
-                end = i
-                flag = true
-            }
-            else { end = i }
-        }
-        else {
-            if end - start >= 1 {
-                arr.removeSubrange(start..<end)
-                i = start
-                end = start
-            }
-            flag = false
-        }
-        i += 1
-        if i >= arr.count {
-            if end - start >= 1 { arr.removeSubrange(start..<end) }
-            break
-        }
-    }
+    var answer = String(new_id)
     
-    //step4
-    if arr.first == "." { arr.removeFirst() }
-    if arr.last == "." { arr.removeLast() }
-    
-    //step5
-    if arr.isEmpty { arr.append("a") }
-    
-    //step6
-    if arr.count >= 16 {
-        arr.removeSubrange(15...)
-        if arr.last == "." { arr.removeLast() }
-    }
-    
-    //step7
-    while arr.count <= 2 { arr.append(arr.last!) }
-    
-    var answer = String()
-    for ch in arr { answer.append(ch) }
+    //step 1
+    answer = answer.lowercased()
+    //step 2
+    answer.removeAll(where: { !$0.isLetter && !$0.isNumber && !"-_.".contains($0) })
+    //step 3
+    while answer.contains("..") { answer = answer.replacingOccurrences(of: "..", with: ".") }
+    //step 4
+    while answer.first == "." { answer.removeFirst() }
+    while answer.last == "." { answer.removeLast() }
+    //step 5
+    if answer.count == 0 { answer = "a" }
+    //step 6
+    while answer.count >= 16 || answer.last == "." { answer.removeLast() }
+    //step 7
+    while answer.count <= 2 { answer.append(answer.last!) }
     return answer
 }
