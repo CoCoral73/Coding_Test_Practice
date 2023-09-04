b97a8f27855839b6e74a5bb4315c476f847b8c56
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-09-04 14:38:14 +0900

    [unrated] Title: 이차원 배열 대각선 순회하기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ board:[[Int]], _ k:Int) -> Int {
+    var answer = 0
+    for i in 0..<board.count {
+        for j in 0..<board[i].count where i + j <= k { answer += board[i][j] }
+    }
+    return answer
+}
