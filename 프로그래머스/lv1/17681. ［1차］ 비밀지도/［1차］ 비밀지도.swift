3990f6019262f22728d1a5c0da1de52742cb5063
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-09-04 16:31:36 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,12 +1,11 @@
+import Foundation
+
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     var answer = [String]()
     for i in 0..<n {
-        var line1 = String(arr1[i], radix: 2)
-        line1 = line1.count == n ? line1 : String(repeating: "0", count: n-line1.count) + line1
-        var line2 = String(arr2[i], radix: 2)
-        line2 = line2.count == n ? line2 : String(repeating: "0", count: n-line2.count) + line2
-        answer.append(zip(line1, line2).reduce("") { return $0 + (($1.0 == "0" && $1.1 == "0") ? String(" ") : String("#")) })
-        
+        var line = String(arr1[i] | arr2[i], radix: 2)
+        line = String(repeating: "0", count: n - line.count) + line
+        answer.append(line.reduce("") { return $0 + ($1 == "0" ? " " : "#") })
     }
     return answer
 }
