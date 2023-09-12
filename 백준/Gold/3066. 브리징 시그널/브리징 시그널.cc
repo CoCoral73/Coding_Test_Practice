07a86555252db3d6cdd95af93fbfe6266089a0e9
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-09-12 17:01:46 +0900

    [Gold II] Title: 브리징 시그널, Time: 24 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/3066. 브리징 시그널/브리징 시그널.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#define fastIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastIO;
+
+    int T, N, tmp;
+    vector<int> stock, result;
+    cin >> T;
+    for (int i = 1; i <= T; i++) {
+        cin >> N;
+
+        for (int j = 0; j < N; j++) {
+            cin >> tmp;
+            stock.emplace_back(tmp);
+        }
+
+        result.emplace_back(stock[0]);
+        for (int j = 1; j < N; j++) {
+            if (result.back() < stock[j])
+                result.emplace_back(stock[j]);
+            else {
+                int s = 0, e = result.size() - 1;
+                int index;
+                while (s <= e) {
+                    int mid = (s + e) / 2;
+                    if (result[mid] >= stock[j]) {
+                        e = mid - 1;
+                        index = mid;
+                    } else
+                        s = mid + 1;
+                }
+                result[index] = stock[j];
+            }
+        }
+        cout << result.size() << '\n';
+        stock.clear();
+        result.clear();
+    }
+}
