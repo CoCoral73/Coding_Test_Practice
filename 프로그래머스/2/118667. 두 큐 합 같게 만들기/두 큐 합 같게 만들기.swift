875797512cc4d0e9cf194f5afd385bf42c94747a
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-11-19 18:50:20 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 81.06 ms, Memory: 26.5 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
@@ -3,17 +3,9 @@ import Foundation
 class Queue<T> {
     private var front_index: Int
     var front: T?
-    var back: T?
     var Q: [T]
     var count: Int
     
-    init() {
-        front_index = 0
-        front = nil
-        back = nil
-        Q = [T]()
-        count = 0
-    }
     init(_ arr: [T]) {
         front_index = 0
         front = arr.first!
@@ -21,7 +13,6 @@ class Queue<T> {
         count = arr.count
     }
     func push(_ x:T) {
-        back = x
         Q.append(x)
         count += 1
     }
@@ -34,28 +25,30 @@ class Queue<T> {
 }
 
 func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
-    if (queue1.reduce(0, +) + queue2.reduce(0, +)) % 2 != 0 { return -1 }
-    var queue = Queue<(q1: Queue<Int>, s1: Int64, q2: Queue<Int>, s2: Int64, count: Int)>()
+    var Q1 = Queue(queue1), Q2 = Queue(queue2), size = queue1.count * 2
+    var sum1 = queue1.reduce(0, +), sum2 = queue2.reduce(0, +)
     
-    queue.push((q1: Queue(queue1), s1: queue1.reduce(Int64(0), { $0 + Int64($1) }), q2: Queue(queue2), s2: queue2.reduce(Int64(0), { $0 + Int64($1) }), count: 0))
-    while queue.count != 0 {
-        let (q1, s1, q2, s2, count) = queue.pop()
-        if s1 == s2 { return count }
-        if count > (q1.count + q2.count) * 2 { break }
-        if s1 < s2 {
-            let front2 = q2.pop()
-            q1.push(front2)
-            if q2.count != 0 {
-                queue.push((q1: q1, s1: s1+Int64(front2), q2: q2, s2: s2-Int64(front2), count: count+1))
-            }
+    if (sum1 + sum2) % 2 == 1 { return -1 }
+    
+    var answer = 0
+    while true {
+        if answer > size * 2 { break }
+        if sum1 < sum2 {
+            let front2 = Q2.pop()
+            Q1.push(front2)
+            sum1 += front2
+            sum2 -= front2
+        }
+        else if sum1 > sum2 {
+            let front1 = Q1.pop()
+            Q2.push(front1)
+            sum1 -= front1
+            sum2 += front1
         }
         else {
-            let front1 = q1.pop()
-            q2.push(front1)
-            if q1.count != 0 {
-                queue.push((q1: q1, s1: s1-Int64(front1), q2: q2, s2: s2+Int64(front1), count: count+1))
-            }
+            return answer
         }
+        answer += 1
     }
     return -1
 }
