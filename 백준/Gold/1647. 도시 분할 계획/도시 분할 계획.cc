3c873a8f7b086e2354dbd82bf04543676c502c2d
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-11-30 15:19:07 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 576 ms, Memory: 39672 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -16,14 +16,15 @@ using namespace std;
 
 class BJ {
     int N, M;
-    vector<pair<int, int>> routes[100001];
-    bool visited[100001];
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    int parent[100001];
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
     int answer = 0;
     int MAX = 0;
 public:
     BJ();
-    void prim(int start);
+    void kruskal();
+    int findParent(int x);
+    void unionParent(int x, int y);
 };
 
 BJ::BJ() {
@@ -31,37 +32,38 @@ BJ::BJ() {
     int a, b, c;
     while (M--) {
         cin >> a >> b >> c;
-        routes[a].emplace_back(c, b);
-        routes[b].emplace_back(c, a);
-        visited[a] = false;
-        visited[b] = false;
+        pq.emplace(c, a, b);
+        parent[a] = a;
+        parent[b] = b;
     }
 
-    prim(a);
+    kruskal();
     cout << answer - MAX;
 }
-void BJ::prim(int start) {
-    visited[start] = true;
-    for (pair<int, int> p : routes[start]) {
-        pq.emplace(p);
-    }
-
-    int edge = 0;
+void BJ::kruskal() {
+    int a, b, c;
     while (!pq.empty()) {
-        pair<int, int> top = pq.top();
+        tie(c, a, b) = pq.top();
         pq.pop();
 
-        if (visited[top.second]) continue;
-        visited[top.second] = true;
-        answer += top.first;
-        MAX = max(MAX, top.first);
-        if (edge == N-1) break;
-
-        for (pair<int, int> p : routes[top.second]) {
-            if (visited[p.second]) continue;
-            pq.emplace(p);
-        }
+        if (findParent(a) == findParent(b)) continue;
+        unionParent(a, b);
+        answer += c;
+        MAX = max(MAX, c);
     }
+}
+int BJ::findParent(int x) {
+    if (parent[x] == x)
+        return x;
+    return findParent(parent[x]);
+}
+void BJ::unionParent(int x, int y) {
+    int px = findParent(x);
+    int py = findParent(y);
+    if (px <= py)
+        parent[py] = px;
+    else
+        parent[px] = py;
 }
 
 signed main(){
