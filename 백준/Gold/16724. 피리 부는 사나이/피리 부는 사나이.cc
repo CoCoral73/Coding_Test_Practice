24dd8f562c4927511cc9cc21d3d5ac02697b269e
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2023-12-10 13:00:52 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 44 ms, Memory: 18644 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
@@ -19,56 +19,45 @@ using namespace std;
 class BJ {
     int N, M;
     char grid[1000][1000];
-    bool visited[1000][1000];
     pii **parent;
+    int answer;
 public:
     BJ();
-    void dfs(int r, int c);
     pii findParent(pii p);
     void unionParent(pii p1, pii p2);
 };
 
 BJ::BJ() {
     cin >> N >> M;
+    answer = N*M;
     parent = new pii *[N];
     for (int i = 0; i < N; i++) {
         parent[i] = new pii[M];
         for (int j = 0; j < M; j++) {
             cin >> grid[i][j];
-            visited[i][j] = false;
             parent[i][j] = {i, j};
         }
     }
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            if (visited[i][j]) continue;
-            dfs(i, j);
+            if (parent[i][j] != make_pair(i, j)) continue;
+            pii p;
+            switch (grid[i][j]) {
+                case 'U': p = findParent({i-1, j}); break;
+                case 'D': p = findParent({i+1, j}); break;
+                case 'L': p = findParent({i, j-1}); break;
+                default: p = findParent({i, j+1});
+            }
+            if (p != make_pair(i, j)) {
+                parent[i][j] = p;
+                answer--;
+            }
         }
     }
+    cout << answer;
+}
 
-    set<pii> s;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++)
-            s.emplace(findParent({i, j}));
-    }
-    cout << s.size();
-}
-void BJ::dfs(int r, int c) {
-    int dr, dc;
-    switch (grid[r][c]) {
-        case 'U': dr = -1, dc = 0; break;
-        case 'D': dr = 1, dc = 0; break;
-        case 'L': dr = 0, dc = -1; break;
-        default: dr = 0, dc = 1; break;
-    }
-    visited[r][c] = true;
-    if (findParent({r+dr, c+dc}) != findParent({r, c}))
-        parent[r][c] = {r+dr, c+dc};
-    if (visited[r+dr][c+dc])
-        return;
-    dfs(r+dr, c+dc);
-}
 pii BJ::findParent(pii p) {
     int r = p.first, c = p.second;
     if (parent[r][c] == make_pair(r, c))
