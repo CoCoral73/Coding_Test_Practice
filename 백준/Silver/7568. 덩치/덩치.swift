90d0379447f785dac16aaeeda62d7ae8f1554d62
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-07-02 13:46:45 +0900

    [Silver V] Title: 덩치, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+let N = fIO.readInt()
+var infos = [(Int, Int, Int)]()
+for _ in 0..<N {
+    let weight = fIO.readInt(), height = fIO.readInt()
+    infos.append((weight, height, 1))
+}
+
+for i in 0..<N {
+    for j in (i+1)..<N {
+        if infos[i].0 < infos[j].0 && infos[i].1 < infos[j].1 {
+            infos[i].2 += 1
+        }
+        else if infos[i].0 > infos[j].0 && infos[i].1 > infos[j].1 {
+            infos[j].2 += 1
+        }
+    }
+}
+infos.forEach { print($0.2, terminator: " ") }
