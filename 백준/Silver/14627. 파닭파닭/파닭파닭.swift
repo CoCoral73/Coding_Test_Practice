fd74592bb0f4957cc6dc16646760bb80ff914d78
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-07-12 15:12:10 +0900

    [Silver II] Title: 파닭파닭, Time: 200 ms, Memory: 101004 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+let S = fIO.readInt(), C = fIO.readInt()
+let green = (0..<S).map { _ in fIO.readInt() }
+var s = 1, e = green.max()!, answer = 0
+while s <= e {
+    let m = (s + e) / 2
+    var count = 0
+    for i in 0..<S {
+        count += green[i] / m
+    }
+    if count >= C {
+        s = m + 1
+        answer = max(answer, m)
+    }
+    else {
+        e = m - 1
+    }
+}
+var c = C, result = 0
+for i in 0..<S {
+    let cnt = min(green[i]/answer, c)
+    result += green[i] - cnt*answer
+    c -= cnt
+}
+print(result)
