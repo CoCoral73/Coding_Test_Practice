d2ccdebe9f8b345b630297cc02b46344238a3978
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-07-17 18:13:51 +0900

    [Silver II] Title: 부분수열의 합, Time: 20 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+let N = fIO.readInt(), S = fIO.readInt()
+let nums = (0..<N).map { _ in fIO.readInt() }
+var answer = 0
+
+func dfs(_ start: Int, _ sum: Int, _ selected: Int) {
+    if sum == S && selected > 0 {
+        answer += 1
+    }
+    
+    for i in start..<N {
+        dfs(i + 1, sum + nums[i], selected + 1)
+    }
+}
+
+dfs(0, 0, 0)
+print(answer)
