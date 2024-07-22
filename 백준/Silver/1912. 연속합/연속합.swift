d409705c559635c2696e7fe9f58b5c1c016f9305
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-07-22 15:36:13 +0900

    [Silver II] Title: 연속합, Time: 20 ms, Memory: 82160 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let n = fIO.readInt(), arr = (0..<n).map { _ in fIO.readInt() }
+var dp = [(Int, Int)](repeating: (0, 0), count: n), answer = arr[0]
+dp[0] = (arr[0], arr[0])
+for i in 1..<n {
+    dp[i] = (arr[i], max(dp[i-1].0, dp[i-1].1) + arr[i])
+    answer = max(max(dp[i].0, dp[i].1), answer)
+}
+print(answer)
