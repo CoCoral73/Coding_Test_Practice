1b03c8b107304fdc3be8064a20f866ec4cbb3e72
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-07-24 14:20:37 +0900

    [Gold V] Title: 용액 합성하기, Time: 36 ms, Memory: 81352 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let N = fIO.readInt(), A = (0..<N).map { _ in fIO.readInt() }
+var ABS = 200000000, answer = 0
+
+for i in 0..<N {
+    var s = 0, e = i - 1
+    while s <= e {
+        let m = (s + e) / 2
+        ABS = min(ABS, abs(A[i] + A[m]))
+        if ABS == abs(A[i] + A[m]) { answer = A[i] + A[m] }
+        if A[i] + A[m] < 0 { s = m + 1 }
+        else if A[i] + A[m] > 0 { e = m - 1 }
+        else { break }
+    }
+    s = i + 1; e = N - 1
+    while s <= e {
+        let m = (s + e) / 2
+        ABS = min(ABS, abs(A[i] + A[m]))
+        if ABS == abs(A[i] + A[m]) { answer = A[i] + A[m] }
+        if A[i] + A[m] < 0 { s = m + 1 }
+        else if A[i] + A[m] > 0 { e = m - 1 }
+        else { break }
+    }
+}
+print(answer)
