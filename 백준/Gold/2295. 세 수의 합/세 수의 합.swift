4879c8c9775b3c29e33ec5e62a0aacf9fb8694f5
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-08-21 15:03:42 +0900

    [Gold IV] Title: 세 수의 합, Time: 128 ms, Memory: 114604 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.swift
@@ -0,0 +1,131 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+class Deque<T> {
+    private var enqueue: [T]
+    private var dequeue: [T] = []
+    
+    var isEmpty: Bool { return enqueue.isEmpty && dequeue.isEmpty }
+    var count: Int { return enqueue.count + dequeue.count }
+    
+    init() { enqueue = [T]() }
+    init(_ arr: [T]) { enqueue = arr }
+    
+    func push_front(_ x: T) { dequeue.append(x) }
+    func push_back(_ x: T) { enqueue.append(x) }
+    func pop_front() -> T? {
+        if dequeue.isEmpty {
+            dequeue = enqueue.reversed()
+            enqueue.removeAll()
+        }
+        return dequeue.popLast()
+    }
+    func pop_back() -> T? {
+        if enqueue.isEmpty {
+            enqueue = dequeue.reversed()
+            dequeue.removeAll()
+        }
+        return enqueue.popLast()
+    }
+    func front() -> T? {
+        if dequeue.isEmpty {
+            dequeue = enqueue.reversed()
+            enqueue.removeAll()
+        }
+        return dequeue.last
+    }
+    func back() -> T? {
+        if enqueue.isEmpty {
+            enqueue = dequeue.reversed()
+            dequeue.removeAll()
+        }
+        return enqueue.last
+    }
+    func clear() { enqueue.removeAll(); dequeue.removeAll() }
+}
+
+let fIO = FileIO()
+
+let N = fIO.readInt(), U = (0..<N).map { _ in fIO.readInt() }.sorted(by: >)
+var twoSum = [Int]()
+for x in 0..<N {
+    for y in 0..<N {
+        twoSum.append(U[x] + U[y])
+    }
+}
+twoSum = Set<Int>(twoSum).sorted()
+
+for k in 0..<N {
+    for z in 0..<N {
+        let find = U[k] - U[z]
+        var s = 0, e = twoSum.count - 1
+        var flag = false
+        while s <= e {
+            let m = (s + e) / 2
+            
+            if twoSum[m] > find { e = m - 1 }
+            else if twoSum[m] < find { s = m + 1 }
+            else { print(U[k]); flag = true; break }
+        }
+        if flag { exit(0) }
+    }
+}
