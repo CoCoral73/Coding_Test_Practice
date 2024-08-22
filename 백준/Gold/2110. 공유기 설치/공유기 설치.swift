19404d9b219b6cdea41f1f66b12b24c794bb4f18
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-08-22 13:49:39 +0900

    [Gold IV] Title: 공유기 설치, Time: 28 ms, Memory: 85384 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let (N, C) = (fIO.readInt(), fIO.readInt())
+let x = (0..<N).map { _ in fIO.readInt() }.sorted()
+
+func check(_ d: Int) -> Bool {
+    var last = x.first!, count = 1
+    for i in 1..<N {
+        if x[i] - last >= d {
+            last = x[i]
+            count += 1
+        }
+    }
+    return count >= C ? true : false
+}
+
+var answer = 0
+var s = 1, e = x.last! - x.first!
+while s <= e {
+    let m = (s + e) / 2
+    
+    if check(m) { s = m + 1; answer = max(answer, m) }
+    else { e = m - 1 }
+}
+print(answer)
