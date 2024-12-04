f977e631ff14b831c0f1219654ebcb4c7a536e84
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-12-04 09:57:09 +0900

    [Silver III] Title: 퇴사, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let N = fIO.readInt()
+let tp = (0..<N).map { _ in (fIO.readInt(), fIO.readInt()) }
+
+var dp = [Int](repeating: 0, count: N+1), answer = 0
+for i in 1...N {
+    let (t, p) = tp[i-1]
+    dp[i] = max(dp[i], dp[i-1])
+    if i+t-1 > N { continue }
+    dp[i+t-1] = max(dp[i+t-1], dp[i-1] + p)
+    answer = max(answer, dp[i+t-1])
+}
+print(answer)
+/*
+Testcase
+ 
+ 
+ */
