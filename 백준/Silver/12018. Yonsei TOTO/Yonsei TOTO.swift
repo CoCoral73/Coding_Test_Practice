e7e5dedce192efe066e08df892ade7c2e86ff7e8
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2024-12-05 20:16:31 +0900

    [Silver III] Title: Yonsei TOTO, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/12018. Yonsei TOTO/Yonsei TOTO.swift
@@ -0,0 +1,90 @@
+import Foundation
+
+//빠른 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let (n, m) = (fIO.readInt(), fIO.readInt())
+var minMileage = [Int]()
+for _ in 0..<n {
+    let (p, l) = (fIO.readInt(), fIO.readInt())
+    let mileage = (0..<p).map { _ in fIO.readInt() }.sorted(by: >)
+    if p >= l {
+        minMileage.append(mileage[l-1])
+    }
+    else { minMileage.append(1) }
+}
+
+minMileage.sort()
+
+var answer = 0, total = 0
+for mileage in minMileage {
+    if total + mileage <= m { answer += 1; total += mileage }
+    else { break }
+}
+print(answer)
+
+/*
+Testcase
+ 
+ 
+ */
