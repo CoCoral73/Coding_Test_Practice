e8f721e33c908b2f9f7bcb9559fc3d9c81e93eb9
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-04-15 10:34:42 +0900

    [level 2] Title: 최솟값 만들기, Time: 6.47 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-func solution(_ A:[Int], _ B:[Int]) -> Int {
-    let len = A.count
-    let A = A.sorted(by: <)
-    let B = B.sorted(by: >)
-    return zip(A, B).reduce(0, { $1.0 * $1.1 + $0 })
+func solution(_ A:[Int], _ B:[Int]) -> Int
+{
+    return zip(A.sorted(by: >), B.sorted(by: <)).reduce(0) { return $0 + $1.0*$1.1 }
 }
