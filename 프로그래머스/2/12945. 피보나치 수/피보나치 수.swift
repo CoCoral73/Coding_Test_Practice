ae921cbdcec335cc3109f261267991dce302bc04
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-04-18 11:49:49 +0900

    [level 2] Title: 피보나치 수, Time: 2.90 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
@@ -1,9 +1,10 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var dp = [0, 1]
+    var fibo: [Int] = [0, 1]
+    
     for i in 2...n {
-        dp.append((dp[i-2] + dp[i-1]) % 1234567)
+        fibo.append((fibo[i-2] + fibo[i-1]) % 1234567)
     }
-    return dp.last!
+    return fibo[n]
 }
