8a8d4d9bef4e9e393db8630dc788dfd336a31426
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-04-18 11:59:18 +0900

    [level 2] Title: 짝지어 제거하기, Time: 205.05 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -1,16 +1,15 @@
 import Foundation
 
 func solution(_ s:String) -> Int{
-    var stack = [Character](repeating: " ", count: s.count), s = Array(s)
-    var top = -1
-    s.forEach {
-        if top == -1 || stack[top] != $0 {
-            top += 1
-            stack[top] = $0
-        }
-        else if stack[top] == $0 {
-            top -= 1
+    var stack: [Character] = []
+    
+    for ch in s {
+        if let last = stack.last, last == ch {
+            stack.removeLast()
+        } else {
+            stack.append(ch)
         }
     }
-    return top == -1 ? 1 : 0
+
+    return stack.isEmpty ? 1:0
 }
