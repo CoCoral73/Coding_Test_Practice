19371e0c012e2e06f555b74ec8ce54004112c3ff
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-04-19 14:26:03 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -1,12 +1,16 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
+func solution(_ n: Int) -> Int {
     var ans:Int = 0, n = n
     
     while n > 0 {
-        if n % 2 == 0 { n /= 2 }
-        else { ans += 1; n = (n - 1) / 2 }
+        if n % 2 == 0 {
+            n /= 2
+        } else {
+            n -= 1
+            ans += 1
+        }
     }
-    
+
     return ans
 }
