15c25533bbd2e89e9fd5dc24a6b20602646ddd73
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-04-29 14:45:53 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
@@ -1,11 +1,19 @@
 import Foundation
 
 func solution(_ n:Int, _ words:[String]) -> [Int] {
-    var set = Set<String>()
+    var setOfWords: Set<String> = []
+    var last: Character? = nil
+    
     for i in 0..<words.count {
-        if set.contains(words[i]) { return [i % n + 1, i / n + 1] }
-        if i > 0 && (words[i-1].suffix(1) != words[i].prefix(1)) { return [i % n + 1, i / n + 1] }
-        set.insert(words[i])
+        let answer = [i % n + 1, i / n + 1]
+        
+        if let last = last, last != words[i].first! || words[i].count == 1 || setOfWords.contains(words[i]) {
+            return answer
+        }
+        
+        setOfWords.insert(words[i])
+        last = words[i].last!
     }
+    
     return [0, 0]
 }
