d388633365a45d27a0051a96560776078f0b1a2f
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-04-30 11:49:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
@@ -2,14 +2,16 @@ import Foundation
 
 func solution(_ n:Int, _ a:Int, _ b:Int) -> Int
 {
-    var answer = 1, a = a, b = b
-    
+    var a = a, b = b
+    if a > b { swap(&a, &b) }
+
+    var answer: Int = 1
     while true {
-        let x = min(a, b), y = max(a, b)
-        if x + 1 == y && x % 2 == 1 { break }
+        if a % 2 == 1 && b == a + 1 { break }
         a = (a + 1) / 2
         b = (b + 1) / 2
         answer += 1
     }
+
     return answer
 }
