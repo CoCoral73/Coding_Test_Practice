846ffdb097263a82e55e2b7458e2348445167528
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-06-05 22:34:50 +0900

    [level 2] Title: 튜플, Time: 1324.18 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.swift
+++ 프로그래머스/2/64065. 튜플/튜플.swift
@@ -1,13 +1,18 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    var arr = String(s[s.index(s.startIndex, offsetBy: 2)...s.index(s.endIndex, offsetBy: -3)]).split(separator: "}").map { $0.hasPrefix(",{") ? $0.suffix($0.count-2) : $0 }
+    var s = s
+    s.removeFirst(2)
+    s.removeLast(2)
+    
+    let str = s.components(separatedBy: "},{").sorted(by: { $0.count < $1.count })
     var answer = [Int]()
-    arr.sort(by: { $0.count < $1.count })
-    arr.forEach {
-        let nums = $0.split(separator: ",").map { Int($0)! }
-        let num = nums.filter { !answer.contains($0) }.first!
+    
+    str.forEach {
+        let nums = Set($0.components(separatedBy: ",").map { Int($0)! })
+        let num = nums.subtracting(answer).first!
         answer.append(num)
     }
+    
     return answer
 }
