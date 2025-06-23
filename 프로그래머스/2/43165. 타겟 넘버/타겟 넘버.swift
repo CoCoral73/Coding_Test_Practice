b43bed536360fa615853cdb27bac75f67ae84917
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-06-24 00:08:15 +0900

    [level 2] Title: 타겟 넘버, Time: 41.58 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -1,17 +1,18 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    let count = numbers.count
-    var answer = 0
+    var answer: Int = 0
     
     func dfs(_ depth: Int, _ sum: Int) {
-        if depth == count {
-            if sum == target { answer += 1 }
-            return
-        }
+        if depth == numbers.count {
+            if sum == target {
+                answer += 1
+            }
+            return 
+        }    
         
-        dfs(depth + 1, sum + numbers[depth])
-        dfs(depth + 1, sum - numbers[depth])
+        dfs(depth+1, sum-numbers[depth])
+        dfs(depth+1, sum+numbers[depth])
     }
     
     dfs(0, 0)
