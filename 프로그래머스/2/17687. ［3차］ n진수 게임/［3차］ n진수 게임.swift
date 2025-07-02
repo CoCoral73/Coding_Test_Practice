9383c59a8849e8845208f9779ee2a95262a211a0
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-07-02 13:29:57 +0900

    [level 2] Title: [3차] n진수 게임, Time: 26.01 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
@@ -1,24 +1,20 @@
-import Foundation
-
-extension String {
-    func getChar(_ i:Int) -> Character {
-        let index = self.index(self.startIndex, offsetBy: i)
-        return self[index]
-    }
-}
+import Foundation 
 
 func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
-    var answer = [String]()
-    var num = 0, order = 0
+    var nums: [String] = []
+    var answer: [String] = []
     
-    while true {
-        let converted = String(num, radix: n, uppercase: true)
-        for i in 0..<converted.count {
-            if order == p-1 { answer.append(String(converted.getChar(i))) }
-            if answer.count == t { return answer.joined() }
-            order = (order + 1) % m
-        }
+    var num: Int = 0
+    while nums.count < m*t - (m - p) {
+        nums += String(num, radix: n, uppercase: true).map { String($0) }
         num += 1
     }
+    
+    var index: Int = p - 1
+    while answer.count < t {
+        answer.append(nums[index])
+        index += m
+    }
+    
     return answer.joined()
 }
