2309d3d7e71227c7383739895b902d5cc3a3f478
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-10-18 21:07:39 +0900

    [level 2] Title: 주차 요금 계산, Time: 14.67 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.swift
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.swift
@@ -1,43 +1,40 @@
 import Foundation
 
-extension String {
-    func substring(_ range:ClosedRange<Int>) -> String {
-        let start = self.index(self.startIndex, offsetBy: range.lowerBound)
-        let end = self.index(self.startIndex, offsetBy: range.upperBound)
-        return String(self[start...end])
-    }
+func convertToMinute(_ s: String) -> Int {
+    let time = s.components(separatedBy: ":").map { Int($0)! }
+    return time[0] * 60 + time[1]
 }
 
-func solution(_ fees: [Int], _ records: [String]) -> [Int] {
-    var INOUT = Dictionary<String, (In: Int, Time: Int)>()
-    var COST = Dictionary<String, Int>()
+func solution(_ fees:[Int], _ records:[String]) -> [Int] {
+    var inTimeByCarNumber: [String: Int] = [:]
+    var accumulatedTimeByCarNumber: [String: Int] = [:]
+    var answer: [Int] = []
     
-    records.forEach { record in
-        let T = Int(record.substring(0...1))! * 60 + Int(record.substring(3...4))!
-        let car = record.substring(6...9)
-        if record.count == 13 {
-            if INOUT[car] == nil { INOUT.updateValue((In: T, Time: 0), forKey: car) }
-            else {
-                INOUT[car]!.In = T
-            }
-            COST.updateValue(0, forKey: car)
-        }
-        else {
-            let time = T - INOUT[car]!.In
-            INOUT[car]!.Time += time
-            INOUT[car]!.In = -1
-        }
-    }
-
-    INOUT.forEach { (key, value) in
-        if value.In != -1 { INOUT[key]!.Time += 23*60 + 59 - value.In }
-        COST[key]! += fees[1]
-        let plus = INOUT[key]!.Time - fees[0]
-        if plus > 0 {
-            let unit_plus = (plus % fees[2]) == 0 ? plus / fees[2] : plus / fees[2] + 1
-            COST[key]! += fees[3] * unit_plus
+    for record in records {
+        let r = record.components(separatedBy: " ")
+        let time = convertToMinute(r[0])
+        
+        if r[2] == "IN" {
+            inTimeByCarNumber[r[1]] = time
+        } else {
+            let accumulated = time - inTimeByCarNumber[r[1]]!
+            accumulatedTimeByCarNumber[r[1], default: 0] += accumulated
+            inTimeByCarNumber.removeValue(forKey: r[1])
         }
     }
     
-    return COST.keys.sorted().map { COST[$0]! }
+    for (car, time) in inTimeByCarNumber {
+        let accumulated = 1439 - time
+        accumulatedTimeByCarNumber[car, default: 0] += accumulated
+    }
+    
+    answer = accumulatedTimeByCarNumber.sorted(by: { $0.key < $1.key }).map {
+        var result: Int = fees[1]
+        let time: Int = max($0.value - fees[0], 0)
+        
+        result += ((time % fees[2] == 0 ? 0 : 1) + time / fees[2]) * fees[3] 
+        return result
+    }
+    
+    return answer
 }
