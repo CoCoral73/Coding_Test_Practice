b650286058218b22a753527babea89cd74950351
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-10-20 12:43:59 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 225.17 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.swift
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.swift
@@ -1,20 +1,20 @@
-import Foundation
+func findHeadAndNumber(_ name: String) -> (String, Int) {
+    let name = Array(name)
+    let first = name.firstIndex { $0.isNumber }!
+    let head = String(name[0..<first]).lowercased()
+    var last = first
+    
+    while last < name.count, name[last].isNumber { last += 1 }
+    let number = Int(String(name[first..<last]))!
+    return (head, number)
+}
 
 func solution(_ files:[String]) -> [String] {
-    var files = files
-    files.sort(by: { (str1, str2) in
-        let idx11 = str1.firstIndex(where: { $0.isNumber })!, idx21 = str2.firstIndex(where: { $0.isNumber })!
-        var idx12 = idx11, idx22 = idx21
+    return files.sorted(by: { (file1, file2) in 
+        let info1 = findHeadAndNumber(file1)
+        let info2 = findHeadAndNumber(file2)
         
-        while idx12 < str1.endIndex && str1[idx12].isNumber { idx12 = str1.index(after: idx12) }
-        while idx22 < str2.endIndex && str2[idx22].isNumber { idx22 = str2.index(after: idx22) }
-        
-        let head1 = str1[str1.startIndex..<idx11].lowercased(), head2 = str2[str2.startIndex..<idx21].lowercased()
-        let number1 = Int(str1[idx11..<idx12])!, number2 = Int(str2[idx21..<idx22])!
-        
-        if head1 == head2 { return number1 < number2 }
-        else { return head1 < head2 }
+        if info1.0 == info2.0 { return info1.1 < info2.1 }
+        else { return info1.0 < info2.0 }
     })
-    
-    return files
 }
