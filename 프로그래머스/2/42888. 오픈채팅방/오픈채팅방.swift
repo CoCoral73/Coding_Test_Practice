f7641caea0c52f3c436f998fc98902e51fb6335d
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2025-10-20 13:04:26 +0900

    [level 2] Title: 오픈채팅방, Time: 744.59 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.swift
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.swift
@@ -1,41 +1,22 @@
 import Foundation
 
 func solution(_ record:[String]) -> [String] {
-    var answer = [String](), index = 0
-    var dict = Dictionary<String, (String, [Int])>()
+    var nicknameById: [String: String] = [:]
     
-    for i in 0..<record.count {
-        let comp = record[i].components(separatedBy: " ")
-        if comp[0] == "Enter" {
-            if dict[comp[1]] == nil {
-                dict.updateValue((comp[2], [index]), forKey: comp[1])
-            }
-            else {
-                if dict[comp[1]]!.0 != comp[2] {
-                    let len = dict[comp[1]]!.0.count
-                    dict[comp[1]]!.1.forEach {
-                        answer[$0] = comp[2] + answer[$0].suffix(answer[$0].count-len)
-                    }
-                    dict[comp[1]]!.0 = comp[2]
-                }
-                dict[comp[1]]!.1.append(index)
-            }
-            answer.append("\(comp[2])님이 들어왔습니다.")
-            index += 1
+    let record: [[String]] = record.map {
+        let str = $0.components(separatedBy: " ")
+        if str.count == 3 {
+            let (id, nickname) = (str[1], str[2])
+            nicknameById[id] = nickname
         }
-        else if comp[0] == "Leave" {
-            let nickname = dict[comp[1]]!.0
-            answer.append("\(nickname)님이 나갔습니다.")
-            dict[comp[1]]!.1.append(index)
-            index += 1
-        }
-        else {
-            let len = dict[comp[1]]!.0.count
-            dict[comp[1]]!.1.forEach {
-                answer[$0] = comp[2] + answer[$0].suffix(answer[$0].count-len)
-            }
-            dict[comp[1]]!.0 = comp[2]
-        }
+        return str
     }
-    return answer
+    
+    return record.map { 
+        if $0[0] == "Enter" {
+            return "\(nicknameById[$0[1]]!)님이 들어왔습니다."
+        } else if $0[0] == "Leave" {
+            return "\(nicknameById[$0[1]]!)님이 나갔습니다."
+        } else { return "" }
+    }.filter { $0 != "" }
 }
