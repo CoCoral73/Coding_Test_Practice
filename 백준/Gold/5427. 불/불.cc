c8cf34f8fe0c360ccac2f43878c9a150f6f3c6d6
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2026-03-20 17:10:51 +0900

    [Gold IV] Title: 불, Time: 92 ms, Memory: 12240 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,112 +1,104 @@
+//
+//  main.cpp
+//  CppPractice
+//
+//  Created by 김정원 on 3/13/26.
+//
+
 #include <iostream>
-#include <cstring>
 #include <vector>
-#include <algorithm>
-#include <queue>
-#include <string>
-#include <stack>
 #include <map>
 #include <set>
-#include <list>
-#include <unordered_set>
-#include <unordered_map>
-#include <tuple>
-#include <limits.h>
-#include <math.h>
-#define int long long
-#define fastio cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
+#include <algorithm>
+#include <queue>
+#define pii pair<int, int>
 
 using namespace std;
 
-int w, h, t;
-char a[1001][1001];
-vector<pair<int, int>> fire;
-pair<int, int> start;
-int visited_fire[1001][1001];
-int visited_sang[1001][1001];
-const int dy[] = { -1, 0, 1, 0 };
-const int dx[] = { 0, 1, 0, -1 };
+const int INF = 2e9;
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
 
-signed main() {
-    fastio;
+int t, w, h;
+vector<string> v;
+int isVisited[1000][1000];
+int isVisitedFire[1000][1000];
 
+queue<pii> q1, q2;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
     cin >> t;
     while (t--) {
-        int chk = 0;
-
-        fire.clear();
-        fill(&visited_fire[0][0], &visited_fire[0][0] + 1001 * 1001, 1e10);
-        fill(&visited_sang[0][0], &visited_sang[0][0] + 1001 * 1001, 0);
-        //fill(&a[0][0], &a[0][0] + 1001 * 1001, '0');
+        fill(&isVisited[0][0], &isVisited[0][0] + 1000 * 1000, INF);
+        fill(&isVisitedFire[0][0], &isVisitedFire[0][0] + 1000 * 1000, INF);
+        
         cin >> w >> h;
-
         for (int i = 0; i < h; i++) {
+            string str;
+            cin >> str;
+            v.emplace_back(str);
+            
             for (int j = 0; j < w; j++) {
-                cin >> a[i][j];
-                if (a[i][j] == '*') fire.push_back({ i, j });
-                if (a[i][j] == '@') start = { i, j };
-            }
-        }
-
-        if (start.first == 0 || start.second == 0 || start.first == h - 1 || start.second == w - 1) {
-            cout << 1 << '\n';
-            continue;
-        }
-
-        queue<pair<int, int>> qt;
-        for (pair<int, int> v : fire) {
-            visited_fire[v.first][v.second] = 1;
-            qt.push(v);
-        }
-        int y, x;
-        while (qt.size()) {
-            tie(y, x) = qt.front(); qt.pop();
-            for (int i = 0; i < 4; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= h || nx >= w) continue;
-                if (visited_fire[ny][nx] != 1e10) continue;
-                if (a[ny][nx] == '#') continue;
-                visited_fire[ny][nx] = visited_fire[y][x] + 1;
-                qt.push({ ny, nx });
-            }
-        }
-        queue<pair<int, int>> q;
-        visited_sang[start.first][start.second] = 1;
-        q.push(start);
-        while (q.size()) {
-            tie(y, x) = q.front(); q.pop();
-            for (int i = 0; i < 4; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= h || nx >= w) continue;
-                if (visited_sang[ny][nx]) continue;
-                if (a[ny][nx] == '#') continue;
-                visited_sang[ny][nx] = visited_sang[y][x] + 1;
-                if (ny == 0 || nx == 0 || ny == h-1 || nx == w-1) {
-                    if (visited_sang[ny][nx] < visited_fire[ny][nx]) {
-                        chk = 1;
-                        cout << visited_sang[ny][nx] << '\n';
-                        break;
-                    }
+                if (str[j] == '*') {
+                    q1.emplace(i, j);
+                    isVisitedFire[i][j] = 1;
+                } else if (str[j] == '@') {
+                    q2.emplace(i, j);
+                    isVisited[i][j] = 1;
                 }
-                q.push({ ny, nx });
             }
-            if (chk) break;
         }
-        if (chk == 0) cout << "IMPOSSIBLE\n";
-
-
+        
+        while (!q1.empty()) {
+            pii front = q1.front();
+            q1.pop();
+            
+            for (int i = 0; i < 4; i++) {
+                int r = front.first + dr[i];
+                int c = front.second + dc[i];
+                
+                if (r < 0 || r >= h || c < 0 || c >= w) continue;
+                if (v[r][c] == '#') continue;
+                if (isVisitedFire[r][c] <= isVisitedFire[front.first][front.second] + 1) continue;
+                
+                isVisitedFire[r][c] = isVisitedFire[front.first][front.second] + 1;
+                q1.emplace(r, c);
+            }
+        }
+        
+        bool flag = false;
+        while (!q2.empty()) {
+            pii front = q2.front();
+            q2.pop();
+            
+            if (front.first == 0 || front.first == h-1 || front.second == 0 || front.second == w-1) {
+                flag = true;
+                cout << isVisited[front.first][front.second] << '\n';
+                break;
+            }
+            
+            for (int i = 0; i < 4; i++) {
+                int r = front.first + dr[i];
+                int c = front.second + dc[i];
+                
+                if (r < 0 || r >= h || c < 0 || c >= w) continue;
+                if (v[r][c] == '#') continue;
+                if (isVisited[r][c] <= isVisited[front.first][front.second] + 1) continue;
+                if (isVisited[front.first][front.second] + 1 >= isVisitedFire[r][c]) continue;
+                
+                isVisited[r][c] = isVisited[front.first][front.second] + 1;
+                q2.emplace(r, c);
+            }
+        }
+        
+        if (!flag) {
+            cout << "IMPOSSIBLE\n";
+        }
+        
+        v.clear();
+        q2 = queue<pii>();
     }
-
-
-
 }
-/*
-1
-4 4
-....
-....
-..@.
-****
- */
